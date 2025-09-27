32423f607b01e5385683868255005ec56010baf1
vickyy234 <vickymayilsamy@gmail.com>
2025-09-27 21:36:07 +0530

    Refactor server loading logic and improve user experience during authentication

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,24 +11,26 @@ import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-  const [serverLoading, setServerLoading] = useState(true);
-  const hasShownServerMessage = useRef(false); // track first-time server wake-up
+  const [showServerWakeMessage, setShowServerWakeMessage] = useState(false);
 
   useEffect(() => {
     const checkUserAuth = async () => {
+      const hasShown = sessionStorage.getItem("serverMessageShown");
+      if (!hasShown) {
+        setShowServerWakeMessage(true);
+      }
       await checkAuth();
-      setServerLoading(false);
-      hasShownServerMessage.current = true;
+      setShowServerWakeMessage(false);
+      sessionStorage.setItem("serverMessageShown", "true");
     };
     checkUserAuth();
-    console.log("Auth user:", authUser);
   }, []);
 
   if (isCheckingAuth && !authUser) {
     return (
       <div className="flex items-center justify-center h-screen flex-col">
         <Loader className="size-10 animate-spin" />
-        {serverLoading && !hasShownServerMessage && (
+        {showServerWakeMessage && (
           <div className="bg-yellow-100 border mx-4 font-semibold border-yellow-300 text-yellow-900 px-4 py-3 rounded-lg shadow-md mt-4 text-sm sm:text-md md:text-lg">
             ⚠️ The server may take up to a minute to wake up if it’s been idle.
             Please wait while it loads.
