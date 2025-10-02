6c8ee240debb0644a636b44d138f60b70f69b069
vickyy234 <vickymayilsamy@gmail.com>
2025-10-02 15:55:16 +0530

    Refactor socket connection handling for improved readability and consistency

--- Server/src/lib/socket.js
+++ Server/src/lib/socket.js
@@ -8,28 +8,29 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
+    credentials: true,
   },
 });
 
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
-    console.log("New client connected", socket.id);
-    
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
-    
+  console.log("New client connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    
-    socket.on("disconnect", () => {
-        console.log("Client disconnected", socket.id);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+  });
 });
 
 export function getReceiverSocketId(userId) {
-    return userSocketMap[userId];
+  return userSocketMap[userId];
 }
 
 export { io, app, server };
