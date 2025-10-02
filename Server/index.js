3c3a16f87c9e4834203174b36929077f2f507ef4
vickyy234 <vickymayilsamy@gmail.com>
2025-10-02 16:41:00 +0530

    Refactor socket initialization by consolidating server and socket setup in socket.js

--- Server/index.js
+++ Server/index.js
@@ -3,14 +3,10 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import http from "http";
 import authRoutes from "./src/routes/authRoutes.js";
 import messageRoutes from "./src/routes/messageRoutes.js";
-import { initSocket } from "./src/lib/socket.js";
+import { app, server } from "./src/lib/socket.js";
 dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -21,11 +17,9 @@ app.use(
     credentials: true,
   })
 );
-initSocket(server, process.env.CLIENT_URL);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 // Server setup + MongoDB connection
 server.listen(process.env.PORT, async () => {
--- Server/src/lib/socket.js
+++ Server/src/lib/socket.js
@@ -1,33 +1,36 @@
 import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
 
 const userSocketMap = {};
 
-export let io;
+io.on("connection", (socket) => {
+  console.log("New client connected", socket.id);
 
-export function initSocket(server, CLIENT_URL) {
-  io = new Server(server, {
-    cors: {
-      origin: CLIENT_URL,
-      credentials: true,
-    },
-  });
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
 
-  io.on("connection", (socket) => {
-    console.log("New client connected", socket.id);
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
-
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected", socket.id);
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
   });
-}
+});
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
+
+export { io, app, server };
